989815392682444e99845df219b3c350cc8ae24a
peaceGiant <pecegiant@gmail.com>
2023-12-04 08:16:32 +0100

    Day 4

---
+++ src/AoC04/Day4.java
@@ -0,0 +1,58 @@
+package AoC04;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day4 {
+
+    public static int getPoints(String line) {
+        String[] parts = line.split("\\|");
+        String[] numbers = parts[0].split(":")[1].trim().split("\\s+");
+        List<Integer> winNumbers = Arrays.stream(numbers).map(Integer::parseInt).toList();
+        numbers = parts[1].trim().split("\\s+");
+        List<Integer> myNumbers = Arrays.stream(numbers).map(Integer::parseInt).toList();
+
+        return (int) Math.pow(2, myNumbers.stream().filter(winNumbers::contains).count() - 1);
+    }
+
+    public static int getScratchCards(String line) {
+        String[] parts = line.split("\\|");
+        String[] numbers = parts[0].split(":")[1].trim().split("\\s+");
+        List<Integer> winNumbers = Arrays.stream(numbers).map(Integer::parseInt).toList();
+        numbers = parts[1].trim().split("\\s+");
+        List<Integer> myNumbers = Arrays.stream(numbers).map(Integer::parseInt).toList();
+
+        return (int) myNumbers.stream().filter(winNumbers::contains).count();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:\\Users\\PeceG\\IdeaProjects\\adventOfCode\\adventOfCode\\src\\AoC04\\data");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        List<String> lines = br.lines().toList();
+
+        int result = lines.stream().mapToInt(Day4::getPoints).sum();
+        System.out.println(result);
+
+        int numCards = lines.size();
+        int[] weights = new int[numCards + 1];
+        Arrays.fill(weights, 1);
+
+        int sum = 0;
+        for (int i = 0; i < numCards; ++i) {
+            int scratchCards = getScratchCards(lines.get(i));
+            for (int j = i + 1; j <= i + scratchCards && j < numCards; ++j) {
+                weights[j] += weights[i];
+            }
+            sum += weights[i];
+        }
+
+        System.out.println(sum);
+
+    }
+
+}
